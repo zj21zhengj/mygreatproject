985eb8cfee780c185691ee2b77274bf29d7ee703
zhengjian <570470242@qq.com>
2019-10-27 23:33:52 +0800

    Reflact 的学习

---
+++ src/main/java/com/liaoxuefeng/javalearnning/Hello.java
@@ -0,0 +1,16 @@
+package com.liaoxuefeng.javalearnning;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Hello {
+    public static void main(String[] args) {
+        Logger logger = Logger.getGlobal();
+        logger.info("start process...");
+        logger.warning("memory is running out...");
+        logger.fine("ignored.");
+        logger.severe("process will be terminated...");
+    }
+}
+
