8907c67676d9a77d77de55f615854045cb444f53
zhengjian <570470242@qq.com>
2019-11-06 00:02:37 +0800

    廖雪峰learan go on

---
+++ src/main/java/com/liaoxuefeng/javalearnning/MainList.java
@@ -0,0 +1,33 @@
+package com.liaoxuefeng.javalearnning;
+import java.util.*;
+
+public class MainList {
+    public static void main(String[] args) {
+        // 构造从start到end的序列：
+        final int start = 10;
+        final int end = 20;
+        List<Integer> list = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            list.add(i);
+        }
+        // 随机删除List中的一个元素:
+        int removed = list.remove((int) (Math.random() * list.size()));
+        int found = findMissingNumber(start, end, list);
+        System.out.println(list.toString());
+        System.out.println("missing number: " + found);
+        System.out.println(removed == found ? "测试成功" : "测试失败");
+    }
+
+    static int findMissingNumber(int start, int end, List<Integer> list) {
+        int ret = start;
+        for(Integer i : list){
+            if(i == ret && ret < end) {
+                ret += + 1;
+            } else {
+                break;
+            }
+        }
+        return ret;
+    }
+
+}
