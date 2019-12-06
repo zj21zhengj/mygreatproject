b07c33f8d0046152f39e8f48cddd6c7ed6df8f49
zhengjian <570470242@qq.com>
2019-12-07 01:01:29 +0800

    模拟layui open

--- src/main/java/com/zj/website/action/HelloController.java
+++ src/main/java/com/zj/website/action/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -40,6 +41,27 @@ public class HelloController{
     @RequestMapping(value="/hello3",method = RequestMethod.GET)
     public String printHello4() {
         return "bankAccount";
+    }
+
+    @RequestMapping(value="/hello5",method = RequestMethod.GET)
+    public String printHello5(Model model) {
+        model.addAttribute("message","Hello!");
+        return "hello";
+    }
+
+    @RequestMapping(value="/hello6",method = RequestMethod.GET)
+    public String printHello6(Model model, @RequestParam("id") String id) {
+        System.out.println(id);
+        String[] sm = id.split(",");
+        for(String s : sm) {
+            String[] ss = s.split("-");
+            if(ss.length > 1) {
+                System.out.println("id" + ss[0]);
+                System.out.println("age" + ss[1]);
+            }
+        }
+        model.addAttribute("id",id);
+        return "hello2";
     }
 
     @RequestMapping(value="/getFlow",method = RequestMethod.GET)
