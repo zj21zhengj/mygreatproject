938c2c46732dd88fe2d647353d0c4cf9f269483c
zhengjian <570470242@qq.com>
2020-03-12 21:25:48 +0800

    注释

--- src/main/java/ioooc/wukong/flowcontrol/CountDownLatchDemo1.java
+++ src/main/java/ioooc/wukong/flowcontrol/CountDownLatchDemo1.java
@@ -4,22 +4,25 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
+/**
+ * 一个线程等待多个线程都执行完毕，再继续自己的工作
+ */
 public class CountDownLatchDemo1 {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        CountDownLatch latch = new CountDownLatch(6);
-        for(int i=0;i<5;i++) {
-            int j = i+1;
+        CountDownLatch latch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            int j = i + 1;
             Runnable runable = new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        Thread.sleep(100);
-                        System.out.println("No"+j+"完成了检查");
+                        Thread.sleep((long) (Math.random()*1000));
+                        System.out.println("No" + j + "完成了检查");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }
-                    finally {
+                    } finally {
+                        //完成后减1
                         latch.countDown();
                     }
                 }
