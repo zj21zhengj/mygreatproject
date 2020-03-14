892ab5a20cdb0d549ec7738e8c3007c43371dc12
zhengjian <570470242@qq.com>
2020-03-15 01:22:48 +0800

    注释

--- src/main/java/com/designpattern/sigleton/Mgr06.java
+++ src/main/java/com/designpattern/sigleton/Mgr06.java
@@ -2,7 +2,8 @@ package com.designpattern.sigleton;
 
 public class Mgr06 {
 
-    //volatite作用 1线程间可见 2防止指令重排
+    //volatite作用 1线程间可见 2防止指令重排  这里是防止指令重排 马士兵公开课美团面试有讲
+    //到，这里是因为会指向半初始化的原因
     private static volatile Mgr06 INSTANCE;
 
     private Mgr06() {
@@ -10,7 +11,7 @@ public class Mgr06 {
 
     public static Mgr06 getInstance() {
         if (INSTANCE == null) {
-            //双重检查  是有必要的，但是太复杂了
+            //双重检查  是有必要的，但是太复杂了 也叫dcl  double check lock
             synchronized (Mgr06.class) {
                 if (INSTANCE == null) {
                     try {
