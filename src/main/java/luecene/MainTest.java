05a3f1921314567f7c6d556e6d1776df5a2c58dc
郑健 <570470242@qq.com>
2020-05-12 15:02:33 +0800

    流式排序

--- src/main/java/luecene/MainTest.java
+++ src/main/java/luecene/MainTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class MainTest {
     public static void main(String[] args) {
 
-        String content = "为什么晚上不能照镜子";
+        String content = "为什么晚上不能照镜子？";
 
         List<String> keywordList = HanLP.extractKeyword(content, 5);
 
